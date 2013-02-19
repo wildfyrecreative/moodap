5a1216f8a8fbcb0585dc553046a8f04272fc468f
Rich Manalang <rmanalang@atlassian.com>
2013-02-18 20:27:16 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Moodapp::Application.config.secret_token = '7e1925d4d54e7ff52a8b166ad8126e44caf4bee6a259d2eada8002cd9f4008df06cd79c23635d3112a0b21252099bc5b2c27bd87659b6d6cad39e08d63633a5d'

ae25b5eee91c89d322e64a8374d30e86eb8841ec
Sten Pittet <spittet@atlassian.com>
2013-02-28 13:51:56 +1100

    Redirecting from #index if logged in

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,8 +3,14 @@ class HomeController < ApplicationController
   
   def index
     respond_to do |format|
-      format.html  # index.html.erb
-      format.json  { render :status => 200 }
+      # redirect to dashboard if user is signed_in
+      if user_signed_in?
+        format.html { redirect_to dashboard_path }
+        format.json { render :status => 200 }
+      else
+        format.html  # index.html.erb
+        format.json  { render :status => 200 }
+      end
     end
   end
   
--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -1,24 +1,35 @@
 require 'spec_helper'
 
 describe HomeController do
-
+  
+  def sign_in_helper
+    @user = FactoryGirl.create(:user)
+    sign_in @user
+  end
+  
+  
   describe "GET 'index'" do
     it "should be successful" do
       get 'index'
       response.should be_success
     end
+    
+    it "should redirect to dashboard" do
+      sign_in_helper
+      get 'index'
+      response.should redirect_to (dashboard_path)
+    end
   end
   
   # Testing access to the Dashboard
   describe "Get 'dashboard'" do
-    it "should not be successful" do
+    it "should redirect to sign in page" do
       get 'dashboard'
       response.should redirect_to(new_user_session_path)
     end
     
     it "should be successful" do
-      @user = FactoryGirl.create(:user)
-      sign_in @user
+      sign_in_helper
       
       get 'dashboard'
       response.should be_success

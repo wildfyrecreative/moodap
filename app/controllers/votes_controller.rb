768fad8f0653f6d91cef3736838e909e0a9d7221
Sten Pittet <spittet@atlassian.com>
2013-03-01 13:02:51 +1100

    Added the export of votes in a csv format

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -2,7 +2,31 @@ class VotesController < ApplicationController
   before_filter :authenticate_user!, :except => [:create]
   
   def index
-    @votes = Vote.includes(:survey, :location).order('created_at desc')
+    conditions = {}
+    if params[:survey_id]
+      conditions[:survey_id] = params[:survey_id]
+      @survey = Survey.find(params[:survey_id])
+    end
+    if params[:location_id]
+      conditions[:location_id] = params[:location_id]
+      @location = Location.find(params[:location_id])
+    end
+    @votes = Vote.where(conditions).includes(:survey, :location).order('created_at desc')
+    
+    respond_to do |format|
+      format.html
+      format.csv  do
+        require 'csv'
+        csv_string = CSV.generate do |csv|
+          csv << %w(score location survey created_at)
+          @votes.each do |vote|
+            csv << [vote.label, vote.location.name, vote.survey.name, vote.created_at]
+          end
+        end
+        puts csv_string
+        render :inline => csv_string, :filename => "#{Time.now.year}#{Time.now.month}#{Time.now.day}_votes.csv"
+      end
+    end
   end
   
   def create
